5245aad7edd282c3c07c47df067769c142ebfbc3
Adriel Pinheiro <adrielpin@gmail.com>
2019-04-02 10:57:31 -0300

    batman

---
+++ README.md
@@ -0,0 +1,36 @@
+Teste de Aptidão DELINEA
+====================
+
+Este projeto tem como objetivo a efetivar teste de aptidão pré estabelecido.
+
+Requerimentos
+-------------
+
+  * Node;
+  * Git;
+  * Npm
+  * Angular
+
+InstalaÃ§Ã£o
+----------
+
+Execute na ordem os comandos:
+
+```bash
+$ git clone //todo
+$ cd projeto
+$ npm install
+$ ng serve
+```
+
+UtilizaÃ§Ã£o
+----------
+
+NÃ£o Ã© necessÃ¡rio configurar qualquer ambiente para rodar esta aplicaÃ§Ã£o. Apenas 
+execute este comando para rodar o servidor incluso e acesse o seguinte endereÃ§o
+em seu navegador web <http://localhost:4200>:
+
+```bash
+$ cd teste/
+$ ng serve
+```
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url: 'http://delineaapi.herokuapp.com/candidate/',
+  client_id: 'Rb6yDNb6muY6Wr9iGybl193VzO6BqOuleLGblg14',
+  client_secret:'NjsLaIedGub9LC2xAKHIt7kiN4DiSBLolT74w2PYrOu4PPdRxCNqgZDLS1UlqwSQry2HSmRj21MWcOiKOuLq8UtsD0LBic26SxJAEHqf7AaZ5C6sOSG9WrHf3gOzJkmY'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
